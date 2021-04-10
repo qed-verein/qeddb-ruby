3c63f552a2cdeeb8c252bfdda85488e46d6d6ffa
Tamás Korodi <tamaskorodi1988@gmail.com>
2021-04-10 10:55:50 +0200

    Show/hide entries for people table

--- app/assets/javascripts/table_download_helper.js
+++ app/assets/javascripts/table_download_helper.js
@@ -1,3 +1,14 @@
 function downloadTable(selector) {
 	$(selector).first().table2csv({"excludeColumns": "td[display='none'], td:first-child, th:first-child"});
 }
+
+function updateVisibility(item) {
+	var regex = /show_([a-z_]+)/;
+	match = item.id.match(regex);
+	if(!match) return;
+	selector = '.' + match[1];
+	checked = $(item).is(':checked');
+	if(checked)
+		$(selector).show();
+	else
+		$(selector).hide();}
