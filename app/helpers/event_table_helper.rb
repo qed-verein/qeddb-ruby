ae9bd71fc957d5029dbb12be57c6a3ebe23d1585
Nicholas Schwab <git@nicholas-schwab.de>
2021-09-30 17:51:26 +0200

    Added helper to display checkboxes uniformly and remove autocomplete caching.

---
+++ app/helpers/event_table_helper.rb
@@ -0,0 +1,11 @@
+module EventTableHelper
+
+  def checkbox(id, text, checked)
+    content_tag :label do
+      content_tag :input, {:autocomplete => 'off', :type => 'checkbox', :id => id, :checked => checked} do
+        concat text
+      end
+    end
+  end
+end
+
