8e92f71f84db8508328b1de63551ac99b2bca80b
Nicholas Schwab <git@nicholas-schwab.de>
2022-09-16 15:43:59 +0200

    Add a right to import bank statements only for the treasurer.

--- app/policies/database_policy.rb
+++ app/policies/database_policy.rb
@@ -1,10 +1,17 @@
 class DatabasePolicy < Struct.new(:user, :database)
 	include PunditImplications
 
-	define_implications({
-		:by_admin => [:import, :export]})
+	define_implications(
+		{
+			:by_admin => [:import, :export],
+			:by_treasurer => [:import_banking_statement]
+		}
+	)
 
- 	def initialize(user, object)
+	def initialize(user, object)
+		if user.treasurer?
+			grant :by_treasurer
+		end
 		if user.admin? || user.chairman?
 			grant :by_admin
 		end
