b91a725c1b1bfaa37e5eeedf86b7c871ba0585a8
Nicholas Schwab <git@nicholas-schwab.de>
2024-06-30 15:16:33 +0200

    feat: Add auditor group.

--- app/policies/outstanding_payments_policy.rb
+++ app/policies/outstanding_payments_policy.rb
@@ -1,12 +1,16 @@
 class OutstandingPaymentsPolicy
 	include PunditImplications
 	define_implications({
-		by_treasurer: [:view, :export]
+		by_treasurer: [:view, :export],
+		by_auditor: [:view]
 	})
 
 	def initialize(user, unused)
 		if user.treasurer?
 			grant :by_treasurer
+		end
+		if user.auditor?
+			grant :by_auditor
 		end
 	end
 end
