0d8acfcb9840b9e33aed18a1778d540c0a03eee7
Nicholas Schwab <git@nicholas-schwab.de>
2024-07-04 18:11:22 +0200

    chore: Extract date setting on focus to function.

---
+++ app/assets/javascripts/date_setter.js
@@ -0,0 +1,29 @@
+// @ts-nocheck
+// ^ Enables ts type checking in js files
+
+/**
+ * @param {HTMLInputElement} elem
+ */
+function setToToday(elem) {
+	elem.valueAsDate = new Date();
+}
+
+/**
+ * @param {Object} [param0]
+ * @param {string} param0.selector
+ * @param {(arg0: HTMLInputElement) => void} param0.setter
+ */
+function enableDateSetting({
+	selector = "input[type=date]",
+	setter = setToToday,
+} = {}) {
+	for (const date_elem of $(selector)) {
+		date_elem.addEventListener("focus", (/** @type {FocusEvent}} */ ev) => {
+			/** type {HTMLInputElement} */
+			const elem = ev.target;
+			if (!elem?.value) {
+				setter(elem);
+			}
+		});
+	}
+}
