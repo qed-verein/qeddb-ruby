b5a68ad40a5251778bea0f963bf4181c0bbd649b
Nicholas Schwab <git@nicholas-schwab.de>
2024-07-15 20:14:55 +0200

    chore: Smurf name registration payment categories since helpers are not namespaced.

--- app/helpers/registration_payments_helper.rb
+++ app/helpers/registration_payments_helper.rb
@@ -1,5 +1,5 @@
 module RegistrationPaymentsHelper
-	def payment_categories
+	def registration_payment_categories
 		[:shopping, :talk_gift, :deposit, :organizer_gift, :transportation, :talk_materials]
 	end
 end
