b1c9d189d0a1a319c5d370269c8d5b3f48eb818c
Tamás Korodi <tamaskorodi1988@gmail.com>
2024-08-30 11:40:53 +0200

    Fix: Add migration for creating auditor group

---
+++ db/migrate/20240830093829_add_auditor_group.rb
@@ -0,0 +1,6 @@
+class AddAuditorGroup < ActiveRecord::Migration[6.1]
+  def change
+    Group.create!({id: 9, title: "Kassenprüfer:innen", mode: :editable, program: :auditors,
+        description: "Diese Gruppe enthält die Kassenprüfer:innen des QEDs NUR während der Kassenprüfung"})
+  end
+end
