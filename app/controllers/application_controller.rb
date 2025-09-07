bc602089b7c7dc3580819a7733bfefec9f84592e
Tamás Korodi <tamaskorodi1988@gmail.com>
2025-09-08 01:05:30 +0200

    Show "Record not found" error instead of 500 status code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
 
   rescue_from Pundit::NotAuthorizedError, with: :access_denied_handler
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_founrd_handler
 
   def access_denied_handler
     respond_to do |type|
@@ -20,6 +21,18 @@ class ApplicationController < ActionController::Base
     end
     true
   end
+
+  def record_not_founrd_handler
+    respond_to do |type|
+      type.html do
+        flash[:alert] = 'Record not found.'
+        redirect_to root_path
+      end
+      type.all { render nothing: true, status: :forbidden }
+    end
+    true
+  end
+
 
   def user_for_paper_trail
     logged_in? ? current_user.full_name + " (ID: #{current_user.id})" : 'Anonym'
