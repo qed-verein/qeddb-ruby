38700882f494dca2c582a86d9acb243720acf043
Daniel Kirch <34161969+danielkirch@users.noreply.github.com>
2025-09-09 19:36:15 +0200

    chore: rubocop -A

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,17 @@
 class AdminController < ApplicationController
-    include PolicyHelper
+  include PolicyHelper
 
-    breadcrumb 'admin', :admin_path
+  breadcrumb 'admin', :admin_path
 
-    def enable_privileged_mode
-        session[:mode] = 'privileged'
-        flash[:notice] = t('actions.admin.enter_privileged_mode')
-        redirect_to request.referer || root_path
-    end
+  def enable_privileged_mode
+    session[:mode] = 'privileged'
+    flash[:notice] = t('actions.admin.enter_privileged_mode')
+    redirect_to request.referer || root_path
+  end
 
-    def disable_privileged_mode
-        session[:mode] = 'standard'
-        flash[:notice] = t('actions.admin.exit_privileged_mode')
-        redirect_to root_path
-    end
+  def disable_privileged_mode
+    session[:mode] = 'standard'
+    flash[:notice] = t('actions.admin.exit_privileged_mode')
+    redirect_to root_path
+  end
 end
