e7c1e24dbefa89f9b2aaac9013d3513a359f08d7
Nicholas42 <nics-lohr@gmx.de>
2025-09-09 19:14:13 +0200

    Privileged mode (#1)
    * feat: Add comments to start page if users are not currently in
    privileged mode.
    
    * fix: spelling of "priviliegiert" to "privilegiert"
    
    ---------
    
    Co-authored-by: Nicholas Schwab <git@nicholas-schwab.de>

--- app/helpers/policy_helper.rb
+++ app/helpers/policy_helper.rb
@@ -1,15 +1,15 @@
 module PolicyHelper
-  # ist user ein Admin und im priviliegierten Modus?
+  # ist user ein Admin und im privilegierten Modus?
   def active_admin?(user, mode)
     user&.admin? && mode == 'privileged'
   end
 
-  # ist user ein Chairman und im priviliegierten Modus?
+  # ist user ein Chairman und im privilegierten Modus?
   def active_chairman?(user, mode)
     user&.chairman? && mode == 'privileged'
   end
 
-  # ist user ein Treasurer und im priviliegierten Modus?
+  # ist user ein Treasurer und im privilegierten Modus?
   def active_treasurer?(user, mode)
     user&.treasurer? && mode == 'privileged'
   end
