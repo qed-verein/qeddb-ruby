c3c0990bbe3ac4c0cb0ea565fb056046ac3b068f
Manf Paul <git@manfp.de>
2026-02-07 19:02:03 +0000

    explain why we had to kill the auditor

--- db/migrate/20240830093829_add_auditor_group.rb
+++ db/migrate/20240830093829_add_auditor_group.rb
@@ -1,5 +1,9 @@
 class AddAuditorGroup < ActiveRecord::Migration[6.1]
   def change
+  # this fucks shit up because it creates an object with outdated field names.
+  # we already create an auditor group in seeds.rb for test deployments, and surely
+  # prod has one too. therefore dummied out.
+
   #  Group.create!({ id: 9, title: 'Kassenprüfer:innen', mode: :editable, program: :auditors,
   #                  description: 'Diese Gruppe enthält die Kassenprüfer:innen des QEDs NUR während der Kassenprüfung' })
   end
