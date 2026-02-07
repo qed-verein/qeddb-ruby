75ac763effa9ce0c5a1f24ff326ea42826135118
jbb01 <32650546+jbb01@users.noreply.github.com>
2026-02-07 13:29:24 +0100

    fix: schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -244,6 +244,13 @@ ActiveRecord::Schema.define(version: 2025_10_19_142157) do
   end
 
 
+  create_view "active_affiliations", sql_definition: <<-SQL
+    		-- Liefert die Liste aller momentan gültigen Gruppenzugehörigkeiten
+		SELECT group_id, groupable_type, groupable_id
+		FROM affiliations
+		WHERE (start IS NULL OR start <= CURRENT_TIMESTAMP)
+			AND (end IS NULL OR end >= CURRENT_TIMESTAMP)
+  SQL
   create_view "recursive_subgroups", sql_definition: <<-SQL
     		/* Erzeugt eine SQL-View, welche für jede Gruppe alle hinzugefügten Untergruppen
 		zurückliefert. Dabei werden auch mehrfach ineinander geschachtelte Gruppen berücksichtigt. */
@@ -260,6 +267,38 @@ ActiveRecord::Schema.define(version: 2025_10_19_142157) do
 			FROM direct_subgroups AS d, recursive_subgroups_relation AS r
 			WHERE d.child_id = r.group_id)
 		SELECT * FROM recursive_subgroups_relation
+  SQL
+  create_view "recursive_members", sql_definition: <<-SQL
+      /* Erzeugt eine SQL-View, welche für jede Gruppe alle darin enthaltenen Personen zurückliefert.
+     Dabei werden auch automatisch verwaltete Gruppen sowie  rekursive Beziehungen von
+     verschachtelten Gruppen berücksichtigt */
+  WITH RECURSIVE
+  -- Liste der automatisch eingetragenen Gruppenmitglieder
+  automatic_members(group_id, person_id) AS (
+  	SELECT groups.id, registrations.person_id
+  	FROM groups, registrations
+  	WHERE groups.event_id = registrations.event_id AND
+  		((groups.program = 7 AND registrations.organizer) OR
+  		 (groups.program = 8 AND registrations.status IN (1, 2)))
+  	UNION
+  	SELECT groups.id, people.id
+  	FROM groups, people
+  	WHERE (groups.program = 4 AND people.active AND (CURRENT_TIMESTAMP BETWEEN people.joined AND people.member_until)) OR
+  		  (groups.program = 5 AND people.active AND (NOT CURRENT_TIMESTAMP BETWEEN people.joined AND people.member_until OR
+  				people.joined IS NULL OR people.member_until IS NULL)) OR
+  		  (groups.program = 6 AND people.active AND people.newsletter)),
+  -- Liste der manuell eingetragenen Gruppenmitglieder
+  direct_members(group_id, person_id) AS (
+  	SELECT group_id, groupable_id FROM active_affiliations
+  	WHERE groupable_type = 'Person'
+  	UNION
+  	SELECT * FROM automatic_members),
+  -- Liste aller momentanen Mitgleider einer Gruppe (auch rekursiv durch Untergruppen)
+  recursive_members_relation(group_id, person_id) AS (
+  	SELECT DISTINCT r.group_id, d.person_id
+  	FROM recursive_subgroups AS r, direct_members AS d
+  	WHERE d.group_id = r.descendant_id)
+  SELECT * FROM recursive_members_relation
   SQL
   create_view "accounts", sql_definition: <<-SQL
     		/* Erzeugt eine SQL-View, welche für jede Berechtigunsgruppe
@@ -309,44 +348,5 @@ ActiveRecord::Schema.define(version: 2025_10_19_142157) do
 			SELECT *, TRUE AS m FROM manual UNION
 			SELECT *, FALSE AS m FROM automatic) AS combination_table
 		GROUP BY mailinglist_id, email_address
-  SQL
-  create_view "active_affiliations", sql_definition: <<-SQL
-    		-- Liefert die Liste aller momentan gültigen Gruppenzugehörigkeiten
-		SELECT group_id, groupable_type, groupable_id
-		FROM affiliations
-		WHERE (start IS NULL OR start <= CURRENT_TIMESTAMP)
-			AND (end IS NULL OR end >= CURRENT_TIMESTAMP)
-  SQL
-  create_view "recursive_members", sql_definition: <<-SQL
-      /* Erzeugt eine SQL-View, welche für jede Gruppe alle darin enthaltenen Personen zurückliefert.
-     Dabei werden auch automatisch verwaltete Gruppen sowie  rekursive Beziehungen von
-     verschachtelten Gruppen berücksichtigt */
-  WITH RECURSIVE
-  -- Liste der automatisch eingetragenen Gruppenmitglieder
-  automatic_members(group_id, person_id) AS (
-  	SELECT groups.id, registrations.person_id
-  	FROM groups, registrations
-  	WHERE groups.event_id = registrations.event_id AND
-  		((groups.program = 7 AND registrations.organizer) OR
-  		 (groups.program = 8 AND registrations.status IN (1, 2)))
-  	UNION
-  	SELECT groups.id, people.id
-  	FROM groups, people
-  	WHERE (groups.program = 4 AND people.active AND (CURRENT_TIMESTAMP BETWEEN people.joined AND people.member_until)) OR
-  		  (groups.program = 5 AND people.active AND (NOT CURRENT_TIMESTAMP BETWEEN people.joined AND people.member_until OR
-  				people.joined IS NULL OR people.member_until IS NULL)) OR
-  		  (groups.program = 6 AND people.active AND people.newsletter)),
-  -- Liste der manuell eingetragenen Gruppenmitglieder
-  direct_members(group_id, person_id) AS (
-  	SELECT group_id, groupable_id FROM active_affiliations
-  	WHERE groupable_type = 'Person'
-  	UNION
-  	SELECT * FROM automatic_members),
-  -- Liste aller momentanen Mitgleider einer Gruppe (auch rekursiv durch Untergruppen)
-  recursive_members_relation(group_id, person_id) AS (
-  	SELECT DISTINCT r.group_id, d.person_id
-  	FROM recursive_subgroups AS r, direct_members AS d
-  	WHERE d.group_id = r.descendant_id)
-  SELECT * FROM recursive_members_relation
   SQL
 end
