918e50a34b1884f178321ebf3b4da5d2c723c985
Jana Lemke <dev@jana-lemke.de>
2026-02-08 13:10:46 +0100

    feat: generate reference line and mailing list from title
    Instead of trying to be smart about edge cases and long titles,
    make sure the validation catches them and hands them back to the user
    to decide on something sensible. It should still handle the most common
    cases without intervention.
    
    Also added some more tests.

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
   def default_event_hash
-    { title: 'Testveranstaltung',
+    { title: 'test 2002',
       start: Time.zone.local(2002, 2, 20),
       end: Time.zone.local(2002, 2, 22),
       deadline: Time.zone.local(2002, 1, 1),
@@ -10,8 +10,46 @@ class EventTest < ActiveSupport::TestCase
       cost: 100 }
   end
 
+  def emojis_in_title
+    { title: 'test🐧 2023',
+      start: Time.zone.local(2023, 2, 20),
+      end: Time.zone.local(2023, 2, 22),
+      deadline: Time.zone.local(2002, 1, 1),
+      max_participants: 20,
+      cost: 100 }
+  end
+
+  def umlaute_in_title
+    { title: 'testäöüøåß 2023',
+      start: Time.zone.local(2023, 2, 20),
+      end: Time.zone.local(2023, 2, 22),
+      deadline: Time.zone.local(2002, 1, 1),
+      max_participants: 20,
+      cost: 100,
+      mailinglist_slug: 'umlaute23'
+    }
+  end
+
+  def long_title
+    { title: 'Dampfschifffahrtskapitänsmützenbandakademie 2023',
+      start: Time.zone.local(2023, 2, 20),
+      end: Time.zone.local(2023, 2, 22),
+      deadline: Time.zone.local(2002, 1, 1),
+      max_participants: 20,
+      cost: 100,
+    }
+  end
+
   test 'create_event' do
     assert_nothing_raised { Event.create!(default_event_hash) }
+  end
+
+  test 'emojis are allowed in title' do
+    assert_nothing_raised { Event.create!(emojis_in_title) }
+  end
+
+  test 'umlauts are allowed in title' do
+    assert_nothing_raised { Event.create!(umlaute_in_title) }
   end
 
   test 'update_event' do
@@ -37,4 +75,31 @@ class EventTest < ActiveSupport::TestCase
       Event.create!(default_event_hash.merge(start: Time.zone.local(2002, 2, 22), end: Time.zone.local(2002, 2, 20)))
     end
   end
+
+  test 'reference_line_gets_generated_if_missing' do
+    event = Event.create!(default_event_hash)
+    assert_not_nil(event.reference_line)
+  end
+
+  test 'mailinglist_slug_gets_generated_if_missing' do
+    event = Event.create!(default_event_hash)
+    assert_not_nil(event.mailinglist_slug)
+  end
+
+  test 'mailing_list_generated_from_long_title_complains_about_being_too_long' do
+    error = assert_raise(ActiveRecord::RecordInvalid) do
+      Event.create!(long_title)
+    end
+    assert_match(/#{Event.human_attribute_name(:mailinglist_slug)} ist zu lang/i, error.message)
+  end
+
+  test 'reference_line_generated_from_long_title_complains_about_being_too_long' do
+    error = assert_raise(ActiveRecord::RecordInvalid) do
+      Event.create!(long_title)
+    end
+    assert_match(/#{Event.human_attribute_name(:reference_line)} ist zu lang/i, error.message)
+  end
+
+
+
 end
