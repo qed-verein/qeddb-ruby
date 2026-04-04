5b969319fb37bf1b252399536b39d60fa04379ca
Tamás Korodi <tamaskorodi1988@gmail.com>
2026-04-04 15:59:57 +0200

    Add hooks for custom handlers when creating and deleting events

--- config/initializers/qeddb_hooks.rb
+++ config/initializers/qeddb_hooks.rb
@@ -1,5 +1,8 @@
 # Configurable hooks
 Rails.application.config.qeddb_hooks = {
   person_created: proc { |person| printf "Person created: %s\n", person.account_name },
-  person_deleted: proc { |person| printf "Person deleted: %s\n", person.account_name }
+  person_deleted: proc { |person| printf "Person deleted: %s\n", person.account_name },
+
+  event_created: proc { |event| printf "Event created: %s\n", event.title },
+  event_deleted: proc { |event| printf "Event deleted: %s\n", event.title }
 }
