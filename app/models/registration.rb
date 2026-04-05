b3f8bc405e81fd4daaf92c34ea24689130e6a300
Tamás Korodi <tamaskorodi1988@gmail.com>
2026-04-05 13:51:57 +0200

    Refactor: Use PARTICIPANT_STATUS constant for [:pending, :confirmed] status

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -27,7 +27,8 @@ class Registration < ApplicationRecord
   #             (bspw. um eine zugehörige Zahlung einzutragen)
   enum status: { pending: 1, confirmed: 2, rejected: 3, cancelled: 4, dummy: 5 }
 
-  scope :participant_scope, -> { where(status: [:pending, :confirmed]) } # Muss auch in event.rb angepasst werden
+  PARTICIPANT_STATUS = [:pending, :confirmed]
+  scope :participant_scope, -> { where(status: PARTICIPANT_STATUS) }
 
   # Validierungen
   validates :person, uniqueness: { scope: :event, message:
